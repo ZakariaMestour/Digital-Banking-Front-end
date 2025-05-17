91507400fd206c364de09eb298a7ab7a14a25287
Zakaria <zakaria.mestour-etu@etu.univh2c.ma>
2025-05-17 19:02:20 +0100

    affichage des clients

---
+++ src/app/customers/customers.component.ts
@@ -0,0 +1,30 @@
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+
+import {NgForOf, NgIf} from '@angular/common';
+@Component({
+  selector: 'app-customers',
+  imports: [
+    NgIf,
+    NgForOf
+  ],
+  templateUrl: './customers.component.html',
+  styleUrl: './customers.component.css'
+})
+export class CustomersComponent  implements OnInit {
+  customers: any;
+
+  constructor(private http: HttpClient) {
+  }
+
+  ngOnInit() {
+    this.http.get("http://localhost:8081/api/customers").subscribe({
+      next:(data)=>{
+        this.customers=data;
+      }, error:(err)=>{
+        console.log(err);
+      }
+    });
+
+  }
+}
