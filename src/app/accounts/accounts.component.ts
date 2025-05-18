a6bd6ef06fb9bb1dbd5eb9f7962ce239154468f2
Zakaria <zakaria.mestour-etu@etu.univh2c.ma>
2025-05-18 18:50:07 +0100

    delete + add customers + accounts navigation + operations

--- src/app/accounts/accounts.component.ts
+++ src/app/accounts/accounts.component.ts
@@ -20,6 +20,7 @@ import {AsyncPipe, DecimalPipe, NgClass, NgForOf, NgIf} from '@angular/common';
 })
 export class AccountsComponent implements OnInit{
   accountFromGroup!:FormGroup;
+  operationFormGroup!:FormGroup;
   currentPage:number=0;
   pageSize:number=5;
   accountObservable!:Observable<AccountDetails>;
@@ -28,6 +29,12 @@ export class AccountsComponent implements OnInit{
   ngOnInit() {
     this.accountFromGroup=this.fb.group({
       accountId:this.fb.control('')
+    });
+    this.operationFormGroup=this.fb.group({
+      operationType:this.fb.control(null),
+      amount:this.fb.control(0),
+      description:this.fb.control(null),
+      destination:this.fb.control(null)
     });
 
   }
@@ -40,6 +47,69 @@ export class AccountsComponent implements OnInit{
   goToPage(page: number) {
     this.currentPage=page;
     this.handleSearchAccount()
+
+  }
+
+  handleAccountOperation() {
+    let accountId:string=this.accountFromGroup.value.accountId;
+    let operationType=this.operationFormGroup.value.operationType;
+    if (operationType=='DEBIT'){
+      let req = {
+        accountId: accountId,
+        amount: this.operationFormGroup.value.amount,
+        description: this.operationFormGroup.value.description
+      };
+      this.accountService.debit(req).subscribe({
+        next: (response) => {
+          console.log("Success:", response);
+          alert(response);
+          this.operationFormGroup.reset();
+          this.handleSearchAccount();
+        },
+        error: (err) => {
+          console.error("Error", err);
+          alert(err.value);
+        }
+      });
+    }else if (operationType=='CREDIT'){
+      let req = {
+        accountId: accountId,
+        amount: this.operationFormGroup.value.amount,
+        description: this.operationFormGroup.value.description
+      };
+      this.accountService.credit(req).subscribe({
+        next: (response) => {
+          console.log("Success:", response);
+          alert(response);
+          this.operationFormGroup.reset();
+          this.handleSearchAccount();
+        },
+        error: (err) => {
+          console.error("Error", err);
+          alert(err.value);
+        }
+      });
+
+    }else if (operationType=='TRANSFER'){
+      let req = {
+        sourceAccountId:accountId,
+        destinationAccountId:this.operationFormGroup.value.destination,
+        amount: this.operationFormGroup.value.amount,
+        description: this.operationFormGroup.value.description
+      };
+      this.accountService.transfer(req).subscribe({
+        next: (response) => {
+          console.log("Success:", response);
+          alert(response);
+          this.operationFormGroup.reset();
+          this.handleSearchAccount();
+        },
+        error: (err) => {
+          console.error("Error", err);
+          alert(err.value);
+        }
+      });
+    }
 
   }
 }
--- src/app/services/accounts.service.ts
+++ src/app/services/accounts.service.ts
@@ -12,4 +12,23 @@ export class AccountsService {
   public getAccount(accountId:string,page:number,size:number):Observable<AccountDetails>{
     return this.http.get<AccountDetails>("http://localhost:8081/api/accounts/"+accountId+"/operations?page="+page+"&size="+size);
   }
+  public debit(req: any): Observable<any> {
+    return this.http.post<any>("http://localhost:8081/api/accounts/debit", req, {
+      responseType: 'text' as 'json'
+    });
+  }
+
+  credit(req: any):Observable<any> {
+    return this.http.post<any>("http://localhost:8081/api/accounts/credit",req,{
+      responseType: 'text' as 'json'
+    });
+
+  }
+
+  transfer(req: any):Observable<any> {
+    return this.http.post<any>("http://localhost:8081/api/accounts/transfer",req,{
+      responseType: 'text' as 'json'
+    });
+
+  }
 }
